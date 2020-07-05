b8bdff3077b2a9c63e37d5e6210c7f40ed47001e
annakhuseinova <annakhuseinova@yandex.ru>
2020-07-05 16:38:01 +0300

    Initil commit

---
+++ src/main/java/com/annakhuseinova/apachekafka/controllers/LibraryEventsController.java
@@ -0,0 +1,20 @@
+package com.annakhuseinova.apachekafka.controllers;
+
+import com.annakhuseinova.apachekafka.model.LibraryEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class LibraryEventsController {
+
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent){
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+}
